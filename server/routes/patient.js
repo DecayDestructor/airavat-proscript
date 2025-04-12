baed896862590aa02deaa6e9a856c9020e94c6b1
Decay_Destructor <aaryanmantri29@gmail.com>
2025-04-13 03:27:22 +0530

    frequency modify

--- server/routes/patient.js
+++ server/routes/patient.js
@@ -86,6 +86,7 @@ router.post('/create-prescript', async (req, res) => {
     const medication = medicines.map((medicine, index) => ({
       medicine,
       dosage: Math.min(dosages[index], 100),
+      frequency: frequency[index],
     }))
 
     const newPrescript = new PatientHistory({
@@ -101,7 +102,6 @@ router.post('/create-prescript', async (req, res) => {
       side_effects: [],
       patient_email: email,
       patient_phone: phone,
-      frequency,
       allergy,
       pregnant,
     })
